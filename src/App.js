0f8fe7512cd4c42a6f59625ba4954cbbbfcd9758
Lobo <matheuscardoso1@poli.ufrj.br>
2020-12-08 19:06:27 -0300

    trying to style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
 import ToDos from './components/ToDos';
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import SingleToDo from './components/singletodo'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
